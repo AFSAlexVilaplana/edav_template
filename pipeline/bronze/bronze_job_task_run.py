4c75bda8146027197aaeee22b0b7f14bce57c308
afsalexvilaplana <afsalexvilaplana>
2023-09-19 22:31:13 +0000

    d

--- pipeline/bronze/bronze_job_task_run.py
+++ pipeline/bronze/bronze_job_task_run.py
@@ -11,7 +11,7 @@
 fileName = dbutils.widgets.get("file_name")
 fileExt = dbutils.widgets.get("file_ext")
 tableName = dbutils.widgets.get("table_name")
-print(dbutils.entry_point.getDbutils().notebook().getContext().notebookTaskMetadata.get("prevTask").get("name"))
+print(dbutils.entry_point.getDbutils().notebook().getContext().notebookTaskMetadata().get("prevTask").get("name"))
 parameters = taskParameters(fileName,fileExt,tableName)
 
 # COMMAND ----------
